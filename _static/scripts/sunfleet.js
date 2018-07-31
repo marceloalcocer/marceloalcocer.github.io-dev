c7916d996901c9e83445001f98e54c6ce3d95896
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2018-07-31 21:38:17 +0200

    Update sunfleet prices

--- _static/scripts/sunfleet.js
+++ _static/scripts/sunfleet.js
@@ -10,21 +10,17 @@
 // Module namepace
 var sunfleet= {
 		
-		
 	// Module init
 	init : function(){
 		
 		// Update graph on car/time change
 		function updateGraph(changeEvent){
 			var car = document.getElementById("car").value;
-			var time = document.getElementById("time").value;
-			document.getElementById("graph").src = "_static/img/sunfleet_" + car + "_" + time + ".png";
+			document.getElementById("graph").src = "_static/img/sunfleet_" + car + ".png";
 		}
 		document.getElementById("car").addEventListener( "change", updateGraph, false);
-		document.getElementById("time").addEventListener( "change", updateGraph, false);
 
 	}
-		
 	
 }
 sunfleet.init();
